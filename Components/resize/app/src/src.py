214e294e46f532e4dde09952d46f5acd32b13cb5
Harshitg2306 <hgoel_be19@thapar.edu>
2023-06-29 18:47:48 +0530

    Updated resize function

--- Components/resize/app/src/src.py
+++ Components/resize/app/src/src.py
@@ -3,7 +3,10 @@ import numpy as np
 # import matplotlib.pyplot as plt
 
 
-def resize(image, x=224, y=224):
+def resize(image, x=224, y=None):
+    
+    if y is None:
+        y=x
     # Resize to new pixel size
     new_img = cv2.resize(image, (x,y))
     
